9579e3862289ddd4e01837fdcf0a8a69cad8c0b5
Temur662 <temursayfutdinov3@gmail.com>
2025-02-01 10:33:49 -0500

    Initial Commit

---
+++ CoffeeDrop/CoffeeDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeDropApp.swift
+//  CoffeeDrop
+//
+//  Created by Temurbek Sayfutdinov on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeDropTests/CoffeeDropTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeDropTests.swift
+//  CoffeeDropTests
+//
+//  Created by Temurbek Sayfutdinov on 2/1/25.
+//
+
+import Testing
+@testable import CoffeeDrop
+
+struct CoffeeDropTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
